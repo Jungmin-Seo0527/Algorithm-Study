b7df3b41d2a778053eafe697817831501566b03f
Jungmin-Seo0527 <soato1405@naver.com>
2020-12-19 19:17:18 +0900

    Create BOJ1463_1로만들기.java
    주어진 숫자 N에서 1로 가는 dp를 새움
    
    세가지 조건 중 두가지 3, 2로 나눈 나머지가 0일때 그 수로 나누는 것에 대한 점화식은
    dp[i/num]=min(dp[i/num], dp[i]+1)
    만약 dp[i/num]==0이면 한번도 오지 못한 경우이기 때문에 무조건 넣는다
    현재까지 온 거리와 이전에서 현재로 오는 거리를 비교하여 적은 값을 넣는 마치 최단거리를 구하는 방식으로 dp를 채워 넣는다.
    
    -1을 하는 경우도 위와 마찬가지로 이전의 거리를 내 거리+1과 비교 작은 값을 넣는다.
    dp[i-1]=min(dp[i-1], dp[i]+1)
    
    항상 배열의 범위를 고려하여 인덱스값에 대한 조건을 생각하여야 한다.

---
+++ src/DynamicProgramming/BOJ1463_1로만들기.java
@@ -0,0 +1,60 @@
+
+package DynamicProgramming;
+
+import java.io.*;
+import java.util.*;
+
+// BOJ1463_1로만들기
+public class BOJ1463_1로만들기
+{
+	static int N;
+	static int[ ] dp;
+
+	public static void main(String[ ] args) throws IOException
+	{
+		inputAndSettingData( );
+		solve( );
+	}
+
+	static void inputAndSettingData( ) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine( ));
+		N = Integer.parseInt(st.nextToken( ));
+		dp = new int[ N + 1 ];
+	}
+
+	static void solve( )
+	{
+		for (int i = N; i >= 1; i--)
+		{
+			// 3가지 경우
+			for (int j = 1; j <= 3; j++)
+			{
+				// -1
+				if (j == 1)
+				{
+					if (i - 1 > 0) // 단 확인하려는 곳이 1이상
+					{
+						if ((dp[ i - 1 ] > dp[ i ] + 1) || dp[ i - 1 ] == 0)
+						{
+							dp[ i - 1 ] = dp[ i ] + 1;
+						}
+					}
+				}
+				else // 2또는 3으로 나눌때 나머지가 0이면 그수로 나눔
+				{
+					if (i % j == 0 && i / j > 0)
+					{
+						int temp = i / j;
+						if (dp[ temp ] > dp[ i ] + 1 || dp[ temp ] == 0)
+						{
+							dp[ temp ] = dp[ i ] + 1;
+						}
+					}
+				}
+			}
+		}
+		System.out.println(dp[ 1 ]);
+	}
+}
