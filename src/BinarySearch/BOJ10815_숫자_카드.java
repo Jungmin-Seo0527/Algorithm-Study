be3597ee2b9deb0f2363d87fc1846a7b81b49530
Jungmin-Seo0527 <soato1405@naver.com>
2020-12-27 20:06:25 +0900

    Create BOJ10815_숫자_카드.java
    이분 탐색 문제
    
    BitSet class 를 이용
    주의할 점은 BitSet은 bit의 배열이며 당연히 인덱스 값은 음수가 될수 없다. 따라서 주어진 입력값의 범위가 음수가 포하되어 있으니 항상 0이상으로 만들어 주어야 한다.

---
+++ src/BinarySearch/BOJ10815_숫자_카드.java
@@ -0,0 +1,50 @@
+
+package BinarySearch;
+
+import java.io.*;
+import java.util.*;
+
+// BOJ10815_숫자_카드
+public class BOJ10815_숫자_카드
+{
+	static int N;
+	static BitSet bitset;
+	static int s = 10000000;
+
+	public static void main(String[ ] args) throws IOException
+	{
+		inputAndSettingData( );
+	}
+
+	static void inputAndSettingData( ) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine( ));
+		N = Integer.parseInt(st.nextToken( ));
+		bitset = new BitSet( );
+
+		st = new StringTokenizer(br.readLine( ));
+		for (int i = 0; i < N; i++)
+		{
+			int temp = Integer.parseInt(st.nextToken( ));
+
+			// 주어진 입력값의 범위가 -10000000부터 시작하는데 bitset의 index는 음수를 가질 수 없다.
+			// 따라서 주어지는 입력값에 무조건 10000000을 더해주어 0이상으로 만들어서 setting한다
+			bitset.set(temp + s);
+		}
+
+		st = new StringTokenizer(br.readLine( ));
+		N = Integer.parseInt(st.nextToken( ));
+		st = new StringTokenizer(br.readLine( ));
+
+		StringBuilder sb = new StringBuilder( );
+		for (int i = 0; i < N; i++)
+		{
+			int find = Integer.parseInt(st.nextToken( ));
+			boolean ans = bitset.get(find + s);
+			if (ans) sb.append("1 ");
+			else sb.append("0 ");
+		}
+		System.out.println(sb);
+	}
+}
