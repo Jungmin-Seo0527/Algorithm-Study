7ff21e6bba2b8850821890ee3c14249af0641ccb
Jungmin-Seo0527 <soato1405@naver.com>
2020-12-27 20:04:30 +0900

    Update test.java

---
+++ src/test.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class test
+{
+	public static void main(String[ ] args) throws IOException
+	{
+		inputAndSettingData( );
+	}
+
+	static void inputAndSettingData( ) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine( ));
+	}
+}
