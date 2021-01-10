bd62d253f07c70ef462a0361f3af804ebfd74409
Jungmin-Seo0527 <soato1405@naver.com>
2021-01-11 04:10:55 +0900

    Create BOJ17140_이차원_배열과_연산.java
    조건에서 나타나는 숫자의 횟수를 카운트 하여 새로운 배열을 만드는 과정이 있는데 수의 등장 횟수가 커지는 순으로 숫자와 횟수가 나와야 되는데 만약 횟수가 같은 숫자가 있다면 숫자가 커지는 순으로 나열해야 한다.
    예를 들면 3, 1, 1 이 있으면 3이 1번 1이 2번이므로 3, 1, 1, 2 가 된다 그리고 한번더 수행하면 3이 1번 1이 2번 2가 1번이다 1번 나타나는 수가 3과 2인데 조건에서 커지는 순으로 나와야 하니 2가 먼저 나온다 그래서 결과로 2, 1, 3, 1, 1, 2 가 된다.
    
    처음에는 문제를 보고 우선순위 큐가 떠올랐다. 숫자들을 새고 숫자와 카운트를 저장하는 우선순위 큐로 구현할까 생각했는데 그냥 일반 배열과 linkedList 로 충분히 구현할 수 있다고 생각했다.
    해당 숫자가 나올때마다 카운트를 해주고 카운트 배열을 다시 list에 옮겨 저장하는데 다시 해당 카운트의 행의 list에 숫자를 저장하는 것이다.
    즉 list[숫자의 갯수].add(해당 숫자) 를 횟수를 샌 배열 처음부터 끝까지 수행 한다.
    그러면 list를 처음부터 탐색을 하면 적은 횟수의 적은 숫자부터 출력해낼수 있다. 이러한 방법으로 열 연산과 행 연산을 수행하면 된다.

---
+++ src/Samgsung/BOJ17140_이차원_배열과_연산.java
@@ -0,0 +1,153 @@
+
+package Samgsung;
+
+import java.io.*;
+import java.util.*;
+
+// BOJ17140_이차원_배열과_연산
+public class BOJ17140_이차원_배열과_연산
+{
+	static int graph[][];
+	static int r, c, k, rowSZ, colSZ;
+
+	public static void main(String[ ] args) throws IOException
+	{
+		inputAndSettingData( );
+		int ans = -1;
+		for (int i = 0; i < 101; i++)
+		{
+			if (graph[ r ][ c ] == k)
+			{
+				ans = i;
+				break;
+			}
+			if (rowSZ >= colSZ) rowOp( );
+			else colOp( );
+		}
+		System.out.println(ans);
+	}
+
+	static void rowOp( )
+	{
+		int rs = rowSZ;
+		for (int i = 1; i <= rs; i++)
+		{
+			int cnt[] = new int[ 101 ];
+
+			// 각 숫자를 센다
+			for (int j = 1; j <= colSZ; j++)
+			{
+				cnt[ graph[ i ][ j ] ]++;
+			}
+
+			LinkedList< Integer > list[] = new LinkedList[ 101 ];
+			for (int j = 1; j < 101; j++)
+			{
+				list[ j ] = new LinkedList<>( );
+			}
+
+			// 각 숫자의 횟수에 해당하는 숫자들을 크기순으로 저장
+			for (int j = 1; j < 101; j++)
+			{
+				if (cnt[ j ] == 0) continue; // 0개인것은 고려 안함
+				list[ cnt[ j ] ].add(j); // list[숫자의 갯수].add(해당 숫자) 크기순으로 탐색하므로 당연히 크기순으로 저장
+			}
+
+			int idx = 1; // 그래프를 채우기 위한 인덱스
+			Arrays.fill(graph[ i ], 0); // 미리 해당 열을 0으로 memset
+			for (int j = 1; j < 101; j++)
+			{
+				if (list[ j ].size( ) == 0) continue; // size==0 : 해당 갯수의 숫자는 존재하지 않는다
+				for (int e : list[ j ])
+				{
+					//System.out.print(e + " " + j + "// ");
+					graph[ i ][ idx ] = e;
+					graph[ i ][ idx + 1 ] = j;
+					colSZ = Math.max(colSZ, idx + 1); // colSZ 갱신 (추가되는 인덱스중 가능 큰 인덱스가 됨)
+					idx = idx + 2;
+					if (idx > 100) break; // 100 초과 인덱스부터는 그냥 버림
+				}
+				if (idx > 100) break;
+			}
+		}
+	}
+
+	// rowOp와 행 열만 바꿔주고 동일한 과정 수행
+	static void colOp( )
+	{
+		int cz = colSZ;
+		for (int i = 1; i <= cz; i++)
+		{
+			int cnt[] = new int[ 101 ];
+			for (int j = 1; j <= rowSZ; j++)
+			{
+				cnt[ graph[ j ][ i ] ]++;
+			}
+			LinkedList< Integer > list[] = new LinkedList[ 101 ];
+			for (int j = 1; j < 101; j++)
+			{
+				list[ j ] = new LinkedList<>( );
+			}
+
+			for (int j = 1; j < 101; j++)
+			{
+				if (cnt[ j ] == 0) continue;
+				list[ cnt[ j ] ].add(j);
+			}
+
+			for (int j = 0; j < 101; j++)
+			{
+				graph[ j ][ i ] = 0;
+			}
+
+			int idx = 1;
+			for (int j = 1; j < 101; j++)
+			{
+				if (list[ j ].size( ) == 0) continue;
+				for (int e : list[ j ])
+				{
+					graph[ idx ][ i ] = e;
+					graph[ idx + 1 ][ i ] = j;
+					rowSZ = Math.max(rowSZ, idx + 1);
+					idx = idx + 2;
+					if (idx > 100) break;
+				}
+				if (idx > 100) break;
+			}
+		}
+	}
+
+	static void show( )
+	{
+		StringBuilder sb = new StringBuilder( );
+		for (int i = 1; i <= rowSZ; i++)
+		{
+			for (int j = 1; j <= colSZ; j++)
+			{
+				sb.append(graph[ i ][ j ] + " ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	static void inputAndSettingData( ) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine( ));
+		r = Integer.parseInt(st.nextToken( ));
+		c = Integer.parseInt(st.nextToken( ));
+		k = Integer.parseInt(st.nextToken( ));
+		rowSZ = 3;
+		colSZ = 3;
+		graph = new int[ 101 ][ 101 ];
+		for (int i = 1; i <= 3; i++)
+		{
+			st = new StringTokenizer(br.readLine( ));
+			for (int j = 1; j <= 3; j++)
+			{
+				graph[ i ][ j ] = Integer.parseInt(st.nextToken( ));
+			}
+		}
+	}
+}
