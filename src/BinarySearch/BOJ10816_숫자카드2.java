fe3b31aa0db457beac98a870b6ca3fee746852a7
Jungmin-Seo0527 <soato1405@naver.com>
2021-01-14 02:48:54 +0900

    Create BOJ10816_숫자카드2.java
    이 문제가 왜 이분탐색 카테고리에 있는지는 잘 모르겠다. 각 숫자마다 이분탐색으로 몇개 있는지 세라는 것인가
    
    다른 우수 풀이들도 내 풀이와 비슷한 풀이이다. 이분탐색을 쓰진 않은것 같다.

---
+++ src/BinarySearch/BOJ10816_숫자카드2.java
@@ -0,0 +1,40 @@
+
+package BinarySearch;
+
+import java.io.*;
+import java.util.*;
+
+// BOJ10816_숫자카드2
+public class BOJ10816_숫자카드2
+{
+	static final int PLUS = 10000000;
+	static int arr[] = new int[ PLUS * 2 + 1 ];
+	static StringBuilder sb = new StringBuilder( );
+
+	public static void main(String[ ] args) throws IOException
+	{
+		inputAndSettingData( );
+	}
+
+	static void inputAndSettingData( ) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine( ));
+		int n = Integer.parseInt(st.nextToken( ));
+		st = new StringTokenizer(br.readLine( ));
+		for (int i = 0; i < n; i++)
+		{
+			int temp = Integer.parseInt(st.nextToken( ));
+			arr[ temp + PLUS ]++;
+		}
+		st = new StringTokenizer(br.readLine( ));
+		n = Integer.parseInt(st.nextToken( ));
+		st = new StringTokenizer(br.readLine( ));
+		for (int i = 0; i < n; i++)
+		{
+			int temp = Integer.parseInt(st.nextToken( ));
+			sb.append(arr[ temp + PLUS ] + " ");
+		}
+		System.out.println(sb);
+	}
+}
