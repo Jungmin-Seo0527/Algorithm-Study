e9c34dab721daa2beef995e0890ce4b6b24abedd
Jungmin-Seo0527 <soato1405@naver.com>
2021-01-14 03:51:48 +0900

    Create BOJ1764_듣보잡.java
    이분탐색 문제이지만 해쉬를 이용해서 더 쉽게 풀어냄

---
+++ src/BinarySearch/BOJ1764_듣보잡.java
@@ -0,0 +1,47 @@
+
+package BinarySearch;
+
+import java.io.*;
+import java.util.*;
+
+// BOJ1764_듣보잡
+public class BOJ1764_듣보잡
+{
+	static HashSet< String > hash = new HashSet<>( );
+	static StringBuilder sb = new StringBuilder( );
+
+	public static void main(String[ ] args) throws IOException
+	{
+		inputAndSettingData( );
+	}
+
+	static void inputAndSettingData( ) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine( ));
+		int N = Integer.parseInt(st.nextToken( ));
+		int M = Integer.parseInt(st.nextToken( ));
+		for (int i = 0; i < N; i++)
+		{
+			hash.add(br.readLine( ));
+		}
+		String[ ] arr = new String[ M ];
+		for (int i = 0; i < M; i++)
+		{
+			String temp = br.readLine( );
+			arr[ i ] = new String(temp);
+		}
+		Arrays.sort(arr);
+		int ret = 0;
+		for (int i = 0; i < M; i++)
+		{
+			if (hash.contains(arr[ i ]))
+			{
+				ret++;
+				sb.append(arr[ i ] + "\n");
+			}
+		}
+		System.out.println(ret);
+		System.out.println(sb);
+	}
+}
