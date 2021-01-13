82eee59acd4492b4fa0847055d95d8b3bc09668f
Jungmin-Seo0527 <soato1405@naver.com>
2021-01-14 01:49:56 +0900

    Create BOJ2805_나무_자르기.java
    문제의 카테고리는 이분탐색이나 사실 이분탐색으로 풀어내지 못하였다. 아마 이분탐색으로 풀어낸 풀이보다 처리시간이 길것이라고 예상된다.
    
    가장 큰 나무길이를 시작으로 절단기 길이를 1씩 줄여준다.
    각 과정마다 절단기 보다 큰 나무의 갯수만큼 잘라낸 나무길이를 더해준다.
    위 과정을 계속 반복하여 필요한 나무 길이보다 큰 시점에서의 절단기 길이를 반환한다.
    
    다른 사람들의 풀이를 참조하여 이분탐색으로 풀어내는 방법을 공부해야겠다.

---
+++ src/BinarySearch/BOJ2805_나무_자르기.java
@@ -0,0 +1,61 @@
+
+package BinarySearch;
+
+import java.io.*;
+import java.util.*;
+
+// BOJ2805_나무_자르기
+public class BOJ2805_나무_자르기
+{
+	static int N, M, arr[];
+
+	public static void main(String[ ] args) throws IOException
+	{
+		inputAndSettingData( );
+		System.out.println(solve( ));
+	}
+
+	// 나무를 정렬하고
+	// 가장 큰 나무를 시작으로
+	// 절단기 높이를 -1씩 해주고
+	// 절단기 높이보다 큰 나무의 갯수만큼 + 하면서 절단기 높이를 하나씩 줄이는 방식
+	static int solve( )
+	{
+		int ret = arr[ N - 1 ];
+		int end = N - 1;
+		int sum = 0;
+		while (true)
+		{
+			ret--;
+			if (ret < 0) break;
+			if (end > 0)
+			{
+				while (end > 0)
+				{
+					if (arr[ end - 1 ] > ret) end--;
+					else break;
+				}
+			}
+			sum += (N - end); // 절단기 높이보다 큰 나무의 갯수만큼 더한다
+			//System.out.println(sum);
+			if (sum >= M) return ret;
+		}
+
+		return -1;
+	}
+
+	static void inputAndSettingData( ) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine( ));
+		N = Integer.parseInt(st.nextToken( ));
+		M = Integer.parseInt(st.nextToken( ));
+		arr = new int[ N ];
+		st = new StringTokenizer(br.readLine( ));
+		for (int i = 0; i < N; i++)
+		{
+			arr[ i ] = Integer.parseInt(st.nextToken( ));
+		}
+		Arrays.sort(arr);
+	}
+}
