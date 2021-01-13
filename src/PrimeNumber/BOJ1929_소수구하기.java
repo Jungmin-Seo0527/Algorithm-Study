a4b448a989cb0913afabdd3f701bcf27aba102eb
Jungmin-Seo0527 <soato1405@naver.com>
2021-01-13 20:21:47 +0900

    Create BOJ1929_소수구하기.java
    에라토스테네스의 체를 이용하여 주어진 두수 n1 이상 n2이하의 소수들을 구하는 문제
    특별한것 없음

---
+++ src/PrimeNumber/BOJ1929_소수구하기.java
@@ -0,0 +1,48 @@
+
+package PrimeNumber;
+
+import java.io.*;
+import java.util.*;
+
+// BOJ1929_소수구하기
+public class BOJ1929_소수구하기
+{
+	static boolean prime[];
+
+	public static void main(String[ ] args) throws IOException
+	{
+		inputAndSettingData( );
+	}
+
+	static void solve(int n1, int n2)
+	{
+		StringBuilder sb = new StringBuilder( );
+		prime[ 0 ] = true;
+		prime[ 1 ] = true;
+		for (int i = 2; i * i <= n2; i++)
+		{
+			if (prime[ i ] == false)
+			{
+				for (int j = i * i; j <= n2; j += i)
+				{
+					prime[ j ] = true;
+				}
+			}
+		}
+		for (int i = n1; i <= n2; i++)
+		{
+			if (prime[ i ] == false) sb.append(i + " \n");
+		}
+		System.out.println(sb);
+	}
+
+	static void inputAndSettingData( ) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine( ));
+		int M = Integer.parseInt(st.nextToken( ));
+		int N = Integer.parseInt(st.nextToken( ));
+		prime = new boolean[ N + 1 ];
+		solve(M, N);
+	}
+}
