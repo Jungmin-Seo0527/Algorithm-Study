ad1bad5539c4327f0d678a9bea2c57cb00c8b6bd
Jungmin-Seo0527 <soato1405@naver.com>
2021-01-16 18:40:55 +0900

    Create BOJ1365_꼬인_전깃줄.java
    전깃줄 문제와 마찬가지로 꼬여있는 전깃줄에서 꼬이거나 겹치는 전깃줄들을 최소한으로 없애는 문제
    
    이전 전깃줄 2에서는 없애야 하는 전깃줄 번호도 출력해야 했다면 이 문제는 없애야 하는 전깃줄 갯수만 출력하면 된다.
    전체 전깃줄 길이에서 LIS의 길이만 빼주면 끝

---
+++ src/BinarySearch/BOJ1365_꼬인_전깃줄.java
@@ -0,0 +1,54 @@
+
+package BinarySearch;
+
+import java.io.*;
+import java.util.*;
+
+// BOJ1365_꼬인_전깃줄
+public class BOJ1365_꼬인_전깃줄
+{
+	public static void main(String[ ] args) throws IOException
+	{
+		//inputAndSettingData( );
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine( ));
+		int N = Integer.parseInt(st.nextToken( ));
+		int LIS[] = new int[ N ];
+		int idx = 0;
+
+		st = new StringTokenizer(br.readLine( ));
+		for (int i = 0; i < N; i++)
+		{
+			int here = Integer.parseInt(st.nextToken( ));
+			if (idx == 0)
+			{
+				LIS[ idx++ ] = here;
+			}
+			else
+			{
+				if (LIS[ idx - 1 ] >= here) LIS[ lowerbound(LIS, here, idx) ] = here;
+				else LIS[ idx++ ] = here;
+			}
+		}
+		System.out.println(N - idx);
+	}
+
+	static int lowerbound(int arr[], int target, int len)
+	{
+		int start = 0;
+		int end = len - 1;
+		while (start < end)
+		{
+			int mid = (start + end) >> 1;
+			if (arr[ mid ] >= target) end = mid;
+			else start = mid + 1;
+		}
+		return end;
+	}
+
+	static void inputAndSettingData( ) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine( ));
+	}
+}
