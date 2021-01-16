ada7492f3c355fce51dfd5579d86aead8d40fa00
Jungmin-Seo0527 <soato1405@naver.com>
2021-01-16 16:44:28 +0900

    Create BOJ2352_반도체_설계.java
    LIS 문제들이 실제로 어떻게 표현이 되어 출제되는가를 알수 있는 문제였다.
    
    좌우 양쪽에 포트가 있고 각각 포트번호들이 부여된다.
    주어진 수열, 예를들면 4, 2, 6, 3, 1, 5 가 입력으로 주어진다면 이는 1번포트는 4번포트로 2번포트는 2번포트로 3번포트는 6번 포트로 이어주어야 한다. 즉 본인의 index 번호와 저장된 번호간의 포트가 연결되어야 한다. 각각 포트들은 오름차순으로 정렬이 되어 있으며 포트간 연결시 서로 겹치거나 교차는 불가능 하다.
    이 조건에서 이 문제가 LIS문제라는 것을 떠올려야 한다. 포트 연결선이 서로 겹치거나 교차가 불가능 하니 왼쪽 포트에서 오른쪽 포트로 연결되는 오른쪽 포트의 번호은 오름차순으로 되어있어야 하며 가장 긴 경우의 길이를 구해야 하니 가장 긴 증가하는 부분 수열 즉 LIS문제이다.
    그리고 LIS를 이분탐색을 이용한 lower bound를 이용해서 풀어주면 된다

---
+++ src/BinarySearch/BOJ2352_반도체_설계.java
@@ -0,0 +1,55 @@
+
+package BinarySearch;
+
+import java.io.*;
+import java.util.*;
+
+// BOJ2352_반도체_설계
+// LIS
+public class BOJ2352_반도체_설계
+{
+	public static void main(String[ ] args) throws IOException
+	{
+		//inputAndSettingData( );
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine( ));
+		int N = Integer.parseInt(st.nextToken( ));
+		int LIS[] = new int[ N ];
+		int idx = 0;
+
+		st = new StringTokenizer(br.readLine( ));
+		for (int i = 0; i < N; i++)
+		{
+			int cur = Integer.parseInt(st.nextToken( ));
+			if (idx == 0)
+			{
+				LIS[ idx++ ] = cur;
+			}
+			else
+			{
+				if (LIS[ idx - 1 ] >= cur)
+				{
+					LIS[ lowerbound(LIS, cur, idx) ] = cur;
+				}
+				else
+				{
+					LIS[ idx++ ] = cur;
+				}
+			}
+		}
+		System.out.println(idx);
+	}
+
+	static int lowerbound(int arr[], int target, int len)
+	{
+		int start = 0;
+		int end = len - 1;
+		while (start < end)
+		{
+			int mid = (start + end) >> 1;
+			if (arr[ mid ] >= target) end = mid;
+			else start = mid + 1;
+		}
+		return end;
+	}
+}
