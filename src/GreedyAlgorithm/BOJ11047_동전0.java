103a6ddd9d49dcb6a07f471c4c8e9f8823cb0a34
Jungmin-Seo0527 <soato1405@naver.com>
2021-01-31 04:54:14 +0900

    Create BOJ11047_동전0.java
    쉬운 그리디 알고리즘 문제

---
+++ src/GreedyAlgorithm/BOJ11047_동전0.java
@@ -0,0 +1,47 @@
+
+package GreedyAlgorithm;
+
+import java.io.*;
+import java.util.*;
+
+// BOJ11047_동전0
+// Greedy
+// 동전의 값이 큰것부터 조사
+public class BOJ11047_동전0
+{
+	static int N, K, arr[];
+
+	public static void main(String[ ] args) throws IOException
+	{
+		inputAndSettingData( );
+		solve( );
+	}
+
+	static void solve( )
+	{
+		int ans = 0;
+		for (int i = N - 1; i >= 0; i--)
+		{
+			if (K / arr[ i ] > 0)
+			{
+				ans += K / arr[ i ];
+				K = K % arr[ i ];
+			}
+			if (K == 0) break;
+		}
+		System.out.println(ans);
+	}
+
+	static void inputAndSettingData( ) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine( ));
+		N = Integer.parseInt(st.nextToken( ));
+		K = Integer.parseInt(st.nextToken( ));
+		arr = new int[ N ];
+		for (int i = 0; i < N; i++)
+		{
+			arr[ i ] = Integer.parseInt(br.readLine( ));
+		}
+	}
+}
