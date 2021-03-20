52bafaf7ffbbda4ac47c44b77509a7da3039c67d
Jungmin-Seo0527 <soato1405@naver.com>
2021-03-20 17:35:15 +0900

    gitignore 추가

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        inputAndSettingData();
+        solve();
+    }
+
+    static void solve() {
+
+    }
+
+    static void inputAndSettingData() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+}
--- src/template.java
+++ src/template.java
@@ -2,8 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class template {
+
+
     public static void main(String[] args) throws IOException {
         inputAndSettingData();
+        solve();
+    }
+
+    static void solve() {
+
     }
 
     static void inputAndSettingData() throws IOException {
