9171d08c4a8de1a77c6c5db0f6a8c273b7e48869
Jungmin-Seo0527 <soato1405@naver.com>
2021-03-24 16:30:25 +0900

    Update kakao - 신규_아이디_추천.java

--- src/kakao/recruit2021/신규_아이디_추천.java
+++ src/kakao/recruit2021/신규_아이디_추천.java
@@ -1,4 +1,22 @@
 package kakao.recruit2021;
 
 public class 신규_아이디_추천 {
+    public String solution(String new_id) {
+        String answer = "";
+
+        new_id = new_id.toLowerCase()
+                .replaceAll("[^-_.a-z0-9]", "")
+                .replaceAll("[.]{2,}", ".")
+                .replaceAll("^[.]|[.]$", "");
+        if (new_id.equals("")) {
+            new_id += "a";
+        } else if (new_id.length() >= 16) {
+            new_id = new_id.substring(0, 15)
+                    .replaceAll("^[.]|[.]$", "");
+        }
+        while (new_id.length() <= 2) {
+            new_id += new_id.charAt(new_id.length() - 1);
+        }
+        return new_id;
+    }
 }
