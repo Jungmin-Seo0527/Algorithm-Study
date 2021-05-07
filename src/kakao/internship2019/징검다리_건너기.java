c6164c122f82cd64a0629e6b746a9c65aa5ac9e7
Jungmin-Seo0527 <soato1405@naver.com>
2021-05-07 19:18:57 +0900

    Create 징검다리_건너기 (KAKAO)

--- src/kakao/internship2019/징검다리_건너기.java
+++ src/kakao/internship2019/징검다리_건너기.java
@@ -1,4 +1,28 @@
 package kakao.internship2019;
 
 public class 징검다리_건너기 {
+    public int solution(int[] stones, int k) {
+        int answer = 0;
+        int start = 0;
+        int end = 200_000_000;
+        while (start <= end) {
+            int mid = (start + end) >>> 1;
+            if (!avail(mid, stones, k)) {
+                answer = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return answer;
+    }
+
+    private boolean avail(int m, int[] stones, int k) {
+        int cnt = 0;
+        for (int i = 0; i < stones.length && cnt < k; i++) {
+            if (stones[i] - m <= 0) cnt++;
+            else cnt = 0;
+        }
+        return cnt < k;
+    }
 }
