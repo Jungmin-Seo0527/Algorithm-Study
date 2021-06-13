9675732852d420e3fc630714b70d32b3c5d650e6
Jungmin-Seo0527 <soato1405@naver.com>
2021-06-14 01:06:23 +0900

    Create BOJ1285_동전_뒤집기

--- src/greedy/BOJ1285_동전_뒤집기.java
+++ src/greedy/BOJ1285_동전_뒤집기.java
@@ -1,35 +1,60 @@
 package greedy;
 
 import java.io.*;
+import java.util.*;
 
-// BOJ1285_동전_뒤집기
-/*
-    0과 1을 뒤집어 모두 같은 수가 되도록 할때 뒤집는 횟수를 최소로 하는 방법을 찾는 문제
-    단 연속되어있는 숫자들이 같은 수일 경우에는 한번에 뒤집을 수 있다.
-    즉 0001100 은 가운데 들어있는 11을 한번에 뒤집어서 모든 수가 같도록 만들 수 있다.
-
-    이 문제는 0 덩어리, 1 덩어리의 갯수중 적은 값이 답이다.
-    연속되어 있는 숫자들 즉 덩어리들은 한번에 뒤집기가 가능하니 그 덩어리들의 갯수만큼만 뒤집으면
-    0이든 1이든 모두 같은 숫자로 만들수 있다.
-
- */
 public class BOJ1285_동전_뒤집기 {
+    static int N, ans = Integer.MAX_VALUE;
+    static int[] coins;
+
     public static void main(String[] args) throws IOException {
         inputAndSettingData();
+        solve();
+    }
+
+    static void solve() {
+        doDFS(0);
+        System.out.println(ans);
+    }
+
+    static void doDFS(int row) {
+        if (row == N) {
+            int cnt = 0;
+            for (int i = 0; i < N; i++) {
+                cnt += getLeastCoins(i);
+            }
+            ans = Math.min(ans, cnt);
+            return;
+        }
+        doDFS(row + 1);
+        coins[row] = ~coins[row];
+        doDFS(row + 1);
+    }
+
+    static int getLeastCoins(int col) {
+        int T = 0;
+        int bit = 1 << col;
+        for (int i = 0; i < N; i++) {
+            if ((coins[i] & bit) != 0) {
+                T++;
+            }
+        }
+        return Math.min(T, N - T);
     }
 
     static void inputAndSettingData() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        char cur = input.charAt(0);
-        int[] arr = new int[2];
-        for (int i = 1; i < input.length(); i++) {
-            if (cur == input.charAt(i)) continue;
-            arr[cur - '0']++;
-            cur = input.charAt(i);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        coins = new int[N];
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < line.length(); j++) {
+                char c = line.charAt(j);
+                if (c == 'T') {
+                    coins[i] |= 1 << j;
+                }
+            }
         }
-        arr[cur - '0']++;
-        System.out.println(Math.min(arr[0], arr[1]));
     }
 }
-
